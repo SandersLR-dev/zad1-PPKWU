509161ba24eaf84ac1122218e2d42ba1a38a48f5
SandersLR <praca.dominiksnopek@gmail.com>
2021-10-07 14:08:17 +0200

    initial commit

---
+++ src/main/java/com/example/ppkwuzad1/PpkwuZad1Application.java
@@ -0,0 +1,13 @@
+package com.example.ppkwuzad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuZad1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpkwuZad1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ppkwuzad1/PpkwuZad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppkwuzad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkwuZad1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
