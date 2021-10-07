a7da5d97f944b4586d460aa8d29d6b63cdb50dfb
SandersLR <praca.dominiksnopek@gmail.com>
2021-10-07 14:17:39 +0200

    create controller to get string from site

--- src/main/java/com/example/ppkwuzad1/Controller/ReverseController.java
+++ src/main/java/com/example/ppkwuzad1/Controller/ReverseController.java
@@ -1,7 +1,15 @@
 package com.example.ppkwuzad1.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class ReverseController {
+
+    @GetMapping("/{reverse}")
+    public String reverseString(@PathVariable String reverse){
+        return "abc";
+    }
 }
